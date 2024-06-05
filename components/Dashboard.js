dfd39111bf16b3a2b9fb12e0b03df07a1468b443
doeve <serb_david@yahoo.com>
2024-06-05 16:01:38 +0300

    added devicerows as suppoised

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -14,37 +14,19 @@ import Text from "./CText";
 import DeviceRow from "./DeviceRow";
 import { useNavigation } from "@react-navigation/native";
 import styles from "../assets/styles/styles";
-// import Zeroconf from 'react-native-zeroconf'
-// import PortScanner from 'react-native-find-local-devices';
-import Icon from 'react-native-vector-icons/Ionicons'
-import Ping from 'react-native-ping';
-
-// import CustomToast from "@fredabila/customtoast";
-// import Ping from "@charles-johnson/react-native-ping";
-// import find from 'local-devices'
-// import WTF from "wtf";
-// import find from 'local-devices'
+import Icon from "react-native-vector-icons/Ionicons"
+import Ping from "react-native-ping";
 
 
-// import NetInfo from "@react-native-community/netinfo";
-import {NetworkInfo} from 'react-native-network-info';
+import {NetworkInfo} from "react-native-network-info";
 
-const Dashboard = () => {
+export default Dashboard = () => {
   const navigation = useNavigation();
   const [isLocal, setIsLocal] = useState(true);
   const [ip, setIp] = useState("");
   const [mask, setMask] = useState("");
-  const [devices, setDevices] = useState([1]);
-  // const zeroconf = new Zeroconf()
-
-  // zeroconf.on('stop', () => {
-  //   console.log('[Stop]')
-  // })
-
-  // zeroconf.on('resolved', service => {
-  //   console.log('[Resolve]', JSON.stringify(service, null, 2))
-  // })
-
+  const [devices, setDevices] = useState([]);
+  const [scanning, setScanning] = useState(false);
 
   const subnetToCIDR = (subnet) => {
     const octets = subnet.split('.').map(Number);
@@ -71,26 +53,36 @@ const Dashboard = () => {
   }
 
   const scanAllIPs = async () =>  {
-    console.log('scanning');
-    const maskBits = 32 - parseInt(mask.slice(1), 10);
-    const maskValue = (1 << maskBits) - 1;
-    const networkValue = ip.split('.').reduce((acc, val) => (acc << 8) | Number(val), 0);
+    setScanning(!scanning);
+    console.log(scanning);
+    if (scanning) {
+      console.log('scanning');
+      const maskBits = 32 - parseInt(mask.slice(1), 10);
+      const maskValue = (1 << maskBits) - 1;
+      const networkIp = generateNetworkAddress(ip, mask);
+      const networkValue = networkIp.split('.').reduce((acc, val) => (acc << 8) | Number(val), 0);
 
-    for (let i = 1; i < (1 << maskBits) - 1; i++) {
-      const ipValue = networkValue + i;
-      const ipParts = [
-        (ipValue >> 24) & 255,
-        (ipValue >> 16) & 255,
-        (ipValue >> 8) & 255,
-        ipValue & 255
-      ];
-      const ipAddress = ipParts.join('.');
-      // console.log('IP:', ipAddress);
-      try {
-        const ms = await Ping.start(ipAddress,{ timeout: 1000 });
-        console.log(ipAddress);
-      } catch (error) {
-        console.log('special code',error.code, error.message);
+      for (let i = 1; i < (1 << maskBits) - 1; i++) {
+        const ipValue = networkValue + i;
+        const ipParts = [
+          (ipValue >> 24) & 255,
+          (ipValue >> 16) & 255,
+          (ipValue >> 8) & 255,
+          ipValue & 255
+        ];
+        const ipAddress = ipParts.join('.');
+        if (scanning) {
+          try {
+            const ms = await Ping.start(ipAddress, { timeout: 1000 });
+            console.log([...devices, ipAddress]);
+            setDevices([...devices, ipAddress]);
+            // console.log(devices);
+          } catch (error) {
+            console.log(i, ipAddress,' special code',error.code, error.message);
+          }
+        } else {
+          break;
+        }
       }
     }
   }
@@ -120,32 +112,6 @@ const Dashboard = () => {
       25,
       50,
     );
-  };
-
-  const scan = async () => {
-    console.log('scan');
-    // const scanner = new PortScanner({
-    //   timeout: 40,
-    //   ports: [8000],
-    //   onDeviceFound: (device) => {
-    //     console.log('Found device!', device);
-    //   },
-    //   onFinish: (devices) => {
-    //     console.log('Finished , devices:', devices);
-    //   },
-    //   onCheck: (device) => {
-    //     console.log('Checking IP: ', device.ipAddress);
-    //   },
-    //   onNoDevices: () => {
-    //     console.log('Finished scanning, no results have been found!');
-    //   },
-    //   onError: (error) => {
-    //     // Called when no service found
-    //     console.log('Error', error);
-    //   },
-    // });
-    // scanner.start();
-    // zeroconf.scan(type = 'http', protocol = 'tcp', domain = 'local');
   };
 
   const ShowIp = async () => {
@@ -202,7 +168,7 @@ const Dashboard = () => {
             placeholderTextColor="#606060" 
           />
           <TouchableOpacity style={{...styles.btn, ...{aspectRatio: 1}}} onPress={scanAllIPs}>
-            <Icon name="caret-forward-outline" size={20} color="black" />
+            <Icon name={scanning? "stop" : "caret-forward-outline"} size={20} color="black" />
           </TouchableOpacity>
         </View>
       </View>
@@ -215,19 +181,17 @@ const Dashboard = () => {
         onPress={() => navigation.navigate("Device")}
       />
       <View style={{...styles.content, ...{flexGrow: 1, flexDirection: "column"}}}>
-        <View style={styles.titleContainer}>
-          <Text style={styles.h2}>devices</Text>
+        <View style={{...styles.titleContainer, ...{marginBottom: 7}}}>
+          <Text style={styles.h2}>devices{devices.length? ` (${devices.length})` : ''}</Text>
         </View>
         <ScrollView style={{flexGrow: 1}}>
-          {devices.map((ip) => {
+          {devices.length? devices.map((ip, i) => {
             return (
-              <DeviceRow ip={ip}/>
+              <DeviceRow ip={ip} key={i}/>
             );
-          })}
+          }) : <Text>scan the network</Text>}
         </ScrollView>
       </View>
     </View>
   );
 };
-
-export default Dashboard;
--- components/DeviceRow.js
+++ components/DeviceRow.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
+import {TouchableOpacity} from "react-native";
 import styles from "../assets/styles/styles";
 import Text from "./CText";
+import Icon from "react-native-vector-icons/Ionicons"
 
-
-const DeviceRow = (props) => {
+export default DeviceRow = (props) => {
   const navigation = useNavigation();
 
   return (
-    <TouchableOpacity style={{...styles.btn, ...{flexDirection: "row", justifyContent: "space-between"}}} onPress={() => navigation.navigate("Device", props.ip)}>
+    <TouchableOpacity style={{...styles.btn, ...{flexDirection: "row", justifyContent: "space-between", height: 40, paddingHorizontal: 10}}} onPress={() => navigation.navigate("Device", props.ip)}>
       <Text>{props.ip}</Text>
       <Icon name="caret-forward-outline" size={20} color="black" />
     </TouchableOpacity>
   );
 };
-
-export default DeviceRow;
