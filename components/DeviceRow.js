b3e20f6fb489ffd78fc0c4ec897f5608fb83966f
doeve <serb_david@yahoo.com>
2024-06-07 11:20:53 +0300

    changed color-scheme on dashboard

--- components/DeviceRow.js
+++ components/DeviceRow.js
@@ -9,9 +9,11 @@ export default DeviceRow = (props) => {
   const navigation = useNavigation();
 
   return (
-    <TouchableOpacity style={{...styles.btn, ...{flexDirection: "row", justifyContent: "space-between", height: 40, paddingHorizontal: 10}}} onPress={() => navigation.navigate("Device", props.ip)}>
-      <Text>{props.ip}</Text>
-      <Icon name="caret-forward-outline" size={20} color="black" />
+    <TouchableOpacity style={{...styles.btn, ...{flexDirection: "row", justifyContent: "space-between", height: 40, paddingHorizontal: 10, backgroundColor: "#c0c0c0"}}} onPress={() => navigation.navigate("Device", props.ip)}>
+      <Text style={{fontWeight: 700, color: "#ffffff"}}>{props.ip}</Text>
+      <Icon name="arrow-forward-outline" size={20} color="black" />
     </TouchableOpacity>
   );
 };
+
+//#a9cd9e
