2dc95732e217830057a35bce4c7ad62e0eb6434c
doeve <serb_david@yahoo.com>
2024-06-12 01:15:52 +0300

    changed display of row

--- components/DeviceRow.js
+++ components/DeviceRow.js
@@ -9,10 +9,10 @@ export default DeviceRow = (props) => {
   const navigation = useNavigation();
 
   return (
-    <TouchableOpacity style={{ ...styles.btn, ...{flexDirection: "row", justifyContent: "space-between", height: 40, paddingHorizontal: 10, backgroundColor: "#eeeeee", alignItems: "center", border: 1, borderColor: "#050505"}}} onPress={() => navigation.navigate("Device", props.ip)}>
+    <TouchableOpacity style={{ ...styles.btn, ...{flexDirection: "row", justifyContent: "space-between", height: 40, paddingHorizontal: 10, backgroundColor: "#eeeeee", alignItems: "center", border: 1, borderColor: "#a0a0a0"}}} onPress={() => navigation.navigate("Device", props.ip)}>
       <View style={{ flexDirection: "column"}}>
-        <Text style={{ fontWeight: 700 }}>{props.ip}</Text>
-        <Text style={{ fontSize: 10}}>{props.name}</Text>
+        <Text style={{ fontWeight: 700, fontSize: 15 }}>{props.name}</Text>
+        {props.name !== props.ip && <Text style={{ fontSize: 10, marginBottom: 2 }}>{props.ip}</Text>}
       </View>
       <Icon name="arrow-forward-outline" size={20} color="black" />
     </TouchableOpacity>
